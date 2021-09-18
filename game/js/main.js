c0cead80d8be28ac252e779088dd97482b42f06e
Armando Ramos Sanchez <armandoramossanchenz@gmail.com>
2021-09-19 01:30:56 +0200

    mejoramos movimiento de mario, y añadimos mejoras

--- game/js/main.js
+++ game/js/main.js
@@ -1,6 +1,9 @@
+
+
+
 var juego = document.getElementById("juego");
 
-// juego.style.display="none";
+juego.style.display="none";
 
 //CREACIÓN DEL FONDO
 juego.style.width="100%";
@@ -19,32 +22,92 @@ personaje.style.left="0";
 personaje.style.backgroundImage="url(../game/img/mario-quieto-derecha.png)"
 personaje.style.backgroundSize="contain";
 personaje.style.backgroundRepeat="no-repeat";
-personaje.style.transition="0";
 
 juego.appendChild(personaje);
 
 //MOVIMIENTO DEL PERSONAJE
-let posicionamientoIzquierdo = 0;
-const moverDerecha = () =>{
-    personaje.style.backgroundImage="url(../game/img/mario-quieto-derecha.png)"
-    personaje.style.transition="0.2s";
-    let movimiento = posicionamientoIzquierdo + 6;
-    if (movimiento<=90) {
-        personaje.style.left=movimiento + "%";
-        posicionamientoIzquierdo = posicionamientoIzquierdo + 6;
+var x = 0;
+
+var tecla = undefined;
+console.log(tecla);
+
+
+document.onkeydown = function (e) {
+    tecla=e.key;
+};
+
+document.onkeyup = function (e) {
+    tecla = undefined;
+    contador = 0;
+};
+
+console.log(tecla);
+
+setInterval( mainLoop , 10);
+
+
+var contador = 0;
+
+const saltar = () =>{
+    personaje.style.backgroundImage="url(../game/img/mario-saltando-derecha.png)"
+    personaje.style.transition="0.3s"
+    personaje.style.backgroundSize="contain";
+    personaje.style.backgroundRepeat="no-repeat";
+    personaje.style.bottom = "35%";
+    setTimeout(() => {
+        personaje.style.bottom = "0";
+    }, 400);
+    setTimeout(() => {
+        personaje.style.transition="0s";
+        personaje.style.backgroundImage="url(../game/img/mario-quieto-derecha.png)"        
+    }, 600);
+}
+
+const saltoHorizontal = () =>{
+    personaje.style.backgroundImage="url(../game/img/mario-volando-derecha.png)";
+    personaje.style.transition="0.3s"
+    personaje.style.left = x + 20 + "%";
+    x = x + 20;
+    personaje.style.bottom = "35%";
+    setTimeout(() => {
+        personaje.style.bottom = "0";
+    }, 400);
+    setTimeout(() => {
+        personaje.style.transition="0s";
+        personaje.style.backgroundImage="url(../game/img/mario-quieto-derecha.png)"        
+    }, 600);
+}
+
+function mainLoop() {
+    if(tecla==="d"){
+        x += 0.5;
+        personaje.style.left = x + "%";
+        personaje.style.backgroundImage="url(../game/img/mario-quieto-derecha.png)"
+        setTimeout(() => {
+            personaje.style.backgroundImage="url(../game/img/mario-andando-derecha.png)"
+        }, 50);
+        setTimeout(() => {
+            personaje.style.backgroundImage="url(../game/img/mario-quieto-derecha.png)"
+        }, 200);
+        }
+    if(tecla==="a"){
+        x -= 0.5;
+        personaje.style.left = x + "%";
+        personaje.style.backgroundImage="url(../game/img/mario-quieto-izquierda.png)"
+        setTimeout(() => {
+            personaje.style.backgroundImage="url(../game/img/mario-andando-izquierda.png)"
+        }, 50);
+        setTimeout(() => {
+            personaje.style.backgroundImage="url(../game/img/mario-quieto-izquierda.png)"
+        }, 200);
+    }
+    if(tecla==="w" && contador<1){
+        saltar();
+        contador++;
+    }
+    if(tecla==="f" && contador<1){
+        saltoHorizontal();
+        contador++;
     }
 }
 
-const saltar = () =>{
-    personaje.style.transition="0";
-    personaje.style.backgroundImage="url(../game/img/mario-saltando-derecha.png)"
-    personaje.style.backgroundSize="contain";
-    personaje.style.backgroundRepeat="no-repeat";
-    personaje.style.transition="0.25s";
-    personaje.style.bottom = "22%";
-    setTimeout(() => {
-        personaje.style.bottom = "0";
-        personaje.style.backgroundImage="url(../game/img/mario-quieto-derecha.png)"
-    }, 285);
-    
-}
